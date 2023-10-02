2be80a4d9bdacdf7f914e85a2555b28c4b10e4e7
paulo gabriel <paulo.prado@edu.unipar.br>
2023-10-01 21:34:39 -0300

    .

--- target/generated-sources/annotations/entidades/Estado_.java
+++ target/generated-sources/annotations/entidades/Estado_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-09-29T18:59:36")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-10-01T21:15:00")
 @StaticMetamodel(Estado.class)
 public class Estado_ { 
 
--- target/generated-sources/annotations/entidades/Filiais_.java
+++ target/generated-sources/annotations/entidades/Filiais_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-09-29T18:59:36")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-10-01T21:15:00")
 @StaticMetamodel(Filiais.class)
 public class Filiais_ { 
 

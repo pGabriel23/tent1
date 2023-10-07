bf87cac2f861cbe7e433716f8213350a2e379a64
paulo gabriel <paulo.prado@edu.unipar.br>
2023-10-07 14:15:50 -0300

    relatorios: index

--- target/generated-sources/annotations/entidades/Cliente_.java
+++ target/generated-sources/annotations/entidades/Cliente_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-10-02T18:03:47")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-10-07T14:13:39")
 @StaticMetamodel(Cliente.class)
 public class Cliente_ { 
 
--- target/generated-sources/annotations/entidades/StatusServico_.java
+++ target/generated-sources/annotations/entidades/StatusServico_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-10-02T18:03:47")
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-10-07T14:13:39")
 @StaticMetamodel(StatusServico.class)
 public class StatusServico_ { 
 
